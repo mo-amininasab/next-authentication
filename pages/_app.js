96e31661d891c543a1e6c207e36ac990216a6e0b
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-10-09 22:26:53 +0330

    prevent double session check

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
+import { Provider } from 'next-auth/providers';
+
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    // ? prevents double session checks, but how?
+    <Provider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
