6d4d4ef9a7d78fae93757516f79ead42b480b732
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-10-09 22:56:58 +0330

    change password acion done

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,15 +1,36 @@
+import React, { useRef } from 'react';
+
 import classes from './profile-form.module.css';
 
-function ProfileForm() {
+function ProfileForm({ onChangePassword }) {
+  const oldPasswordRef = useRef();
+  const newPasswordRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredOldPassword = oldPasswordRef.current.value;
+    const enteredNewPassword = newPasswordRef.current.value;
+
+    // Todo: Add validation
+
+    onChangePassword({
+      oldPassword: enteredOldPassword,
+      newPassword: enteredNewPassword,
+    });
+  };
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password" ref={newPasswordRef}>
+          New Password
+        </label>
+        <input type="password" id="new-password" />
       </div>
       <div className={classes.control}>
-        <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <label htmlFor="old-password">Old Password</label>
+        <input type="password" id="old-password" ref={oldPasswordRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
